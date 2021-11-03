5d9e86ee04689671dd49947aa10b1889011fce68
ccccccjh <72149771+Coiggahou2002@users.noreply.github.com>
2021-11-03 21:29:08 +0800

    完成3-8译码器

--- lab1/decoder_38/decoder_38.srcs/sources_1/imports/decoder_38_origin_sources/decoder_38.v
+++ lab1/decoder_38/decoder_38.srcs/sources_1/imports/decoder_38_origin_sources/decoder_38.v
@@ -5,5 +5,23 @@ module decoder_38 (
 	input  wire [2:0] switch,
 	output reg  [7:0] led
 );
+        
+    always @ (*) begin
+        if (enable == 3'b100) begin
+            case (switch)
+                3'b000: led = 8'b1111_1110;
+                3'b001: led = 8'b1111_1101;
+                3'b010: led = 8'b1111_1011;
+                3'b011: led = 8'b1111_0111;
+                3'b100: led = 8'b1110_1111;
+                3'b101: led = 8'b1101_1111;
+                3'b110: led = 8'b1011_1111;
+                3'b111: led = 8'b0111_1111;
+                default: led = 8'b1111_1111;
+            endcase
+        end
+        else led = 8'b1111_1111;
+        
+    end
 
 endmodule
