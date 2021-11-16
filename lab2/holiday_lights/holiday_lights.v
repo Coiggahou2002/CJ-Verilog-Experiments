b77516cd12baaf486d2e41ad1700136c3db86244
ccccccjh <1096119575@qq.com>
2021-11-16 22:18:36 +0800

    节日彩灯bug小修完成
    现在4个时钟周期的仿真和1s周期的上板都通过了

--- lab2/holiday_lights/holiday_lights.v
+++ lab2/holiday_lights/holiday_lights.v
@@ -5,17 +5,34 @@ module holiday_lights (
     input  wire [2:0] switch,
 	output reg  [15:0] led
 );
-reg [36:0] cnt;
+reg [31:0] cnt;
 
 wire rst_n = ~rst;
-reg enable = 1'b0;
-wire cnt_end = (cnt == 37'd4);
-wire cnt_inc = (cnt != 37'd4 && enable);
+
+parameter DEVICE_OFF = 2'b00;
+parameter DEVICE_READY = 2'b01;
+parameter DEVICE_ON = 2'b10;
+parameter DEVICE_PAUSE = 2'b11;
+parameter CLOCK_PERIOD = 32'd100000000;
+parameter LED_SWITCH_TIME = 32'd99999999;
+
+reg [1:0] STATUS = DEVICE_OFF;
+
+wire cnt_end = (cnt == CLOCK_PERIOD);
+wire cnt_inc = (cnt != CLOCK_PERIOD && STATUS == DEVICE_ON);
 
 
 always @(posedge clk or negedge rst_n) begin
-    if (button) begin
-		case (switch)
+    if (~rst_n) begin
+        STATUS = DEVICE_PAUSE;
+    end
+    else if (STATUS == DEVICE_OFF) begin
+        if (button) begin
+            STATUS = DEVICE_READY;
+        end
+    end
+    else if (STATUS == DEVICE_READY) begin
+        case (switch)
             3'b000: led = 16'b0000_0000_0000_0001;
             3'b001: led = 16'b0000_0000_0000_0011;
             3'b010: led = 16'b0000_0000_0000_0111;
@@ -25,12 +42,10 @@ always @(posedge clk or negedge rst_n) begin
             3'b110: led = 16'b0000_0000_0111_1111;
             3'b111: led = 16'b0000_0000_1111_1111;
         endcase
-        enable <= 1'b1;
+        STATUS = DEVICE_ON;
     end
-    
-	if (~rst_n) begin
-        enable <= 1'b0;
-		case (switch)
+    else if (STATUS == DEVICE_PAUSE) begin
+        case (switch)
             3'b000: led = 16'b0000_0000_0000_0001;
             3'b001: led = 16'b0000_0000_0000_0011;
             3'b010: led = 16'b0000_0000_0000_0111;
@@ -40,19 +55,24 @@ always @(posedge clk or negedge rst_n) begin
             3'b110: led = 16'b0000_0000_0111_1111;
             3'b111: led = 16'b0000_0000_1111_1111;
         endcase
+        if (button) begin
+            STATUS = DEVICE_ON;
+        end
     end
-    
-    if (enable) begin
-        if (cnt == 37'd4) begin
-            led <= {led[14:0],led[15]};
+    else if (STATUS == DEVICE_ON) begin
+        if (button) begin
+            STATUS = DEVICE_READY;
+        end
+        else if (cnt == LED_SWITCH_TIME) begin
+            led = { led[14:0], led[15] };
         end
     end
 end
 
 always @(posedge clk or negedge rst_n) begin
-    if (~rst_n) cnt <= 37'd0;
-    else if (cnt_end) cnt <= 37'd0;
-    else if (cnt_inc) cnt <= cnt + 37'd1;
+    if (~rst_n) cnt <= 32'd0;
+    else if (cnt_end) cnt <= 32'd0;
+    else if (cnt_inc) cnt <= cnt + 32'd1;
 end
 
 endmodule
